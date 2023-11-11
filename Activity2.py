f043b1ef42f0b914f88bd9ba779aed0330bd043f
activity2 <jdegano017@gmail.com>
2023-11-11 18:09:55 +0700

    first commit

---
+++ Activity2.py
@@ -0,0 +1,7 @@
+Name= input("What is your name?:")
+Age= input ("How old are you?:")
+Address= input ("What is your address?:")
+
+print("Hello, " + Name)
+print("You are " + Age)
+print("You live in " + Address)
